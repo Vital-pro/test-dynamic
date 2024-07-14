593d663456bceda609e84b02a15f2519820a1f32
Vital-pro <vitupikov@yandex.ru>
2024-07-14 16:49:18 +0300

    added new page test

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
+// 'use client';
 import Image from 'next/image';
+// import React, { useState, useEffect } from 'react';
 
 async function getData() {
   const result = await fetch(process.env.API_URL, {
@@ -14,9 +16,20 @@ async function getData() {
 
 export default async function Home() {
   const data = await getData();
+  // const [state, setState] = useState('');
+
+  // useEffect(() => {
+  //   const result = fetch(process.env.API_URL)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setState(data);
+  //       console.log(data);
+  //       console.log(result);
+  //     });
+  // }, []);
   return (
-    <div className="">
-      <img width={250} height={250} src={data.message} alt="gog" />
+    <div className=''>
+      <img width={250} height={250} src={data.message} alt='dog' />
     </div>
   );
 }
