8c71e09ebd73215d688e513d32b795b370fc9c98
Vital-pro <vitupikov@yandex.ru>
2024-07-29 00:02:01 +0300

    change background with time

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,23 @@
 import Link from 'next/link'
 
+let date = new Date();
+let dateHour = date.getHours();
+
 export const Header = () => {
   return (
-    <header className='header'>
+    <header
+      className='header'
+      style={{
+        borderImage:
+          dateHour >= 4 && dateHour < 17
+            ? 'fill 0 linear-gradient(190deg, #0000, #0000)'
+            : dateHour >= 17 && dateHour < 20
+            ? 'fill 0 linear-gradient(190deg, #0004, #0004)'
+            : dateHour >= 20
+            ? 'fill 0 linear-gradient(190deg, #0009, #0009)'
+            : ''
+      }}
+    >
       <div className='header__container container'>
         <h2 className='header__title'>Солнечные новости</h2>
       </div>
