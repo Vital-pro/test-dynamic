35c61cb03424f1a36339f7881d0cf083209af07d
Vital-pro <vitupikov@yandex.ru>
2024-07-29 20:00:29 +0300

    adjusted the time

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 
 let date = new Date();
-let dateHour = date.getHours();
+let dateHour = (date.getHours() + 3);
 
 export const Header = () => {
   return (
     <header
-      className='header'
-      style={{
-        borderImage:
+    className='header'
+    style={{
+      borderImage:
           dateHour >= 4 && dateHour < 17
             ? 'fill 0 linear-gradient(190deg, #0000, #0000)'
             : dateHour >= 17 && dateHour < 20
